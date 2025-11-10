82fbf8ee299c3bf542d2128d80e605822c0fb87d
Filippo Poltronieri <filippopoltronieri@192.168.1.59>
2025-11-10 15:42:49 +0100

    Commented: deletion

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,14 @@
 class CommentsController < ApplicationController
-  before_action :set_post 
+  before_action :set_post
   # Inseriamo un controllo per assicurarci che solo l'user 
   # che ha scritto il commento possa eliminarlo
+  before_action :set_comment, only: [:destroy]
+  before_action :authorize_comment_deletion, only: [:destroy]
+
+  #
+  #def show
+  #  @comment = @post.comments.find(params[:id])
+  #end
 
   def create
     puts "comment_params: #{comment_params.inspect}"
@@ -11,12 +18,21 @@ class CommentsController < ApplicationController
     else
       redirect_to posts_path, alert: "Failed to create comment."
     end
+  end
 
+  def destroy
+    @comment.destroy
+    redirect_to @post, notice: "Comment was successfully deleted."
   end
 
   private
+
   def set_post
     @post = Post.find(params[:post_id])
+  end
+
+  def set_comment
+    @comment = @post.comments.find(params[:id])
   end
 
   def comment_params 
@@ -24,7 +40,7 @@ class CommentsController < ApplicationController
   end
 
   def authorize_comment_deletion
-    redirect_to url(@post), alert: 'Non puoi rimuovere il commento' unless @comment.user == current_user
+  redirect_to url(@post), alert: 'Non puoi rimuovere il commento' unless @comment.user == Current.user
   end
     
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   belongs_to :user # aggiunta dipendenza con User 
+  broadcasts_to ->(post) { "posts" }
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10, maximum: 128 }
